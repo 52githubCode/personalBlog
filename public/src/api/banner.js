0500921d40d53b53e647c85e1e9b27c10fb561f7
int <3439884587@qq.com>
2022-06-12 23:53:33 +0800

    dist

--- public/src/api/banner.js
+++
@@ -1,5 +0,0 @@
-import request from "@/api/request";
-
-export async function getBanners() {
-    return await request.get("/api/banner")
-}
--- public/src/utils/debounce.js
+++
@@ -1,9 +0,0 @@
-export default function(fn, duration = 100) {
-    let time = null;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn(...args);
-        }, duration);
-    };
-}
